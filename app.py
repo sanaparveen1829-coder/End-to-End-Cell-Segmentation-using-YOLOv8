94d9d88c0dd539019c4d9c131c8ea82f1bc366ed
Sana Parveen V <sanaparveen1829@gmail.com>
2026-02-12 13:22:38 +0530

    data ingestion

---
+++ app.py
@@ -0,0 +1,8 @@
+import os, sys
+from cellSegmentation.pipeline.training_pipeline import TrainPipeline
+
+
+obj=TrainPipeline()
+obj.run_pipeline()
+
+print("Training done")
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cellSegmentation',
+    version='0.0.0',
+    author='Sana Parveen V',
+    author_email='sanaparveen1829@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
