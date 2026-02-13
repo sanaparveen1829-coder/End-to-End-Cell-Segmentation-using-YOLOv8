51e861f0530c875ad6ffbc916d3166bb89c1e271
Sana Parveen V <sanaparveen1829@gmail.com>
2026-02-13 13:16:16 +0530

    data validation

--- cellSegmentation/constant/training_pipeline/__init__.py
+++ cellSegmentation/constant/training_pipeline/__init__.py
@@ -1,3 +1,8 @@
+from dataclasses import field
+from typing import List
+
+
+
 ARTIFACTS_DIR: str = "artifacts"
 
 """
@@ -9,3 +14,12 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/17vDLJrk4iYtysocirKNCDuDIOAsX0-17/view?usp=sharing"
 
+
+"""Data Validation related constant start with DATA_VALIDATION VAR NAME
+    """
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_STATUS_FILE = "validation_status.txt"
+DATA_VALIDATION_ALL_REQUIRED_FILES : list[str] = field(
+        default_factory=lambda: ["train", "valid", "test", "data.yaml"]
+    )
